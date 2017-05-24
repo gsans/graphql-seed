b24c6f555b9ccfaab02be638a818cbcd5fefb0d5
Boubacar Barry <bouba@hackages.io>
2017-05-24 22:19:59 +0200

    Rewrite TopNavigation to stateless component

--- src/components/TopNavigation.js
+++ src/components/TopNavigation.js
@@ -1,24 +1,18 @@
-import React from 'react'
+import React from 'react';
 
-class TopNavigation extends React.Component {
-
-  render() {
-    return (
-      <nav className='navbar navbar-inverse navbar-fixed-top'>
-        <div className='container'>
-          <div className='row'>
-            <div className='col-md-2 col-xs-1' />
-            <div className='col-md-8 col-xs-10'>
-              <div className='centerBlock app-title'>
-                GraphQL Seed <span role="img" aria-label="donut">🍩</span>
-              </div>
-            </div>
-            <div className='col-md-2 col-xs-1' />
+export default () => (
+  <nav className='navbar navbar-inverse navbar-fixed-top'>
+    <div className='container'>
+      <div className='row'>
+        <div className='col-md-2 col-xs-1'/>
+        <div className='col-md-8 col-xs-10'>
+          <div className='centerBlock app-title'>
+            GraphQL Seed
+            <span role="img" aria-label="donut">🍩</span>
           </div>
         </div>
-      </nav>
-    )
-  }
-}
-
-export default TopNavigation
+        <div className='col-md-2 col-xs-1'/>
+      </div>
+    </div>
+  </nav>
+)
